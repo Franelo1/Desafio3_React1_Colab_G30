43439db3a75bd35280ae4b7d537c9cc0cf2f930c
franelo1 <francisco.alorda.c@gmail.com>
2023-04-19 22:11:46 -0400

    primer intento

---
+++ src/Datos/BaseColaboradores.js
@@ -0,0 +1,17 @@
+export const BaseColaboradores = [
+    {
+        id: "1",
+        nombre: "Colaborador 1",
+        correo: "colaborador1@colaboradores.com"
+    },
+    {
+        id: "2",
+        nombre: "Colaborador 2",
+        correo: "colaborador2@colaboradores.com"
+    },
+    {
+        id: "3",
+        nombre: "Colaborador 3",
+        correo: "colaborador3@colaboradores.com"
+    },
+]
